534022c9ec755a567b26c45a850b3e24b812faaa
Jerry-Lindstrom <Jerrylindstrom@hotmail.se>
2014-03-25 19:03:48 +0100

    uppdaterad enligt önskemål

--- Database/database.h
+++ Database/database.h
@@ -16,16 +16,18 @@ class Database {
 	size_t newNewsGroup(std::shared_ptr<NewsGroup> newsGroup);
 	std::shared_ptr<Article> getArticle(size_t articleID, size_t newsGroupID);
 	void deleteArticle(size_t articleID, size_t newsGroupID);
-
 	articleItr newsGroupBegin();
 	articleItr newsGroupEnd();
-
 	void load();
 	void save();
-	
 	std::vector<Article> getArticles(size_t n);
 	size_t deleteNewsGroup(size_t newsGroupID);
 	size_t createNewsGroup(size_t newsGroupID, std::string name);
+	
+	size_t createArticle(size_t nbr, std::string title, std::string author, std::string text);
+	size_t createNG(std::string name);
+	
+	size_t NewsGroupDoesNotExist = 51;
 	size_t NewsGroupExists = 52;
 	size_t ACK = 28; 
 	private:
