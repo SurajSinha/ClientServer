65cecc3691973c00ef41d23cfda83dfed61ab733
SurajSinha <ada10ssi@student.lu.se>
2014-03-29 23:30:08 +0100

    readAnsListNG implemented

--- Client/ans.cc
+++ Client/ans.cc
@@ -1,10 +1,18 @@
 #include "ans.h"
-#include <string>
+
 using namespace std;
 
-void Ans::readAnsListNG(size_t& id, string& name){
-  mh.sendCode(Protocol::ANS_LIST_NG);
-  mh.sendNumParamater(id);
-  mh.sendStringParameter(name);
-  mh.sendCode(Protocol::COM_END);
+vector<NewsGroup> Ans::readAnsListNG(){
+  if(mh.recieveCode()==Protocol::ANS_LIST_NG){
+    vector<NewsGroup> v;
+    int nbrOfItems=mh.recieveIntParameter();
+    for(int i = 0; i != nbrOfItems; ++i){
+      int id=mh.recieveIntParameter();
+      string title=mh.recieveStringParameter();
+      v.push_back(NewsGroup(id,title));
+    }
+    return v;
+  }else{
+    throw ProtocolViolationException();//Ska också client göra detta?
+  }
 }
