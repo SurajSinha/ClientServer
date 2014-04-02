411d4fdaef7c8202b69dba8b6592203c9b27f021
SurajSinha <ada10ssi@student.lu.se>
2014-04-02 20:03:54 +0200

    Testing

--- Client/interface.cc
+++ Client/interface.cc
@@ -8,8 +8,24 @@
 #include "interface.h"
 #include <algorithm>
 using namespace std;
-
-
+string customTrim(const string& str, const string& remove){
+  auto start = str.find_first_not_of(remove);
+  auto end = str.find_last_not_of(remove);
+  return str.substr(start, end);
+}
+int validInput()
+{
+    int x;
+    std::cin >> x;
+    while(std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Bad entry.  Enter a NUMBER: ";
+        std::cin >> x;
+    }
+    return x;
+}
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     cerr << "Usage: myclient host-name port-number" << endl;
@@ -31,7 +47,7 @@ int main(int argc, char* argv[]) {
 
   cout << "Connected to Server ";
   MessageHandler mh;
-  mh.SetConnection(conn);	
+  mh.SetConnection(conn); 
   Com c(mh);
   Ans a(mh); 
   cout << "Welcome to our news system. Type the following in order to execute certain desired tasks:" << endl;
@@ -44,7 +60,7 @@ int main(int argc, char* argv[]) {
 
     cout << "3: Delete newsgroup" <<endl;
 
-    cout << "4: List articles" <<endl;	
+    cout << "4: List articles" <<endl;  
 
     cout << "5: Create article" <<endl;
 
@@ -62,7 +78,7 @@ int main(int argc, char* argv[]) {
       c.sendListNG();
       vector<NewsGroup> g = a.readAnsListNG();
       for(NewsGroup& n: g){
-      	cout << n.name<<endl;
+        cout << n.id << " : "<< n.name<<endl;
       }
       //DONE
 
@@ -71,16 +87,18 @@ int main(int argc, char* argv[]) {
       cout<<"Type in the desired name for the newsgroup:"<<endl;
 
       string name;
+      std::cin.ignore();
       getline(cin,name);
+      name = customTrim(name, " ");
    // Man ska inte kunna skapa en newsgrop med whitespace som namn.
     // namn måste innehålla minst en char.
       
       c.sendCreateNG(name);
       bool done = a.readAnsCreateNG();
       if(done){
-      	cout<<"Newsgroup "<< name <<" created"<<endl;;
+        cout<<"Newsgroup "<< name <<" created"<<endl;;
       }else{
-      	cout<<"NewsGroup with name "<<name<<" already exists in the current database."<<endl;
+        cout<<"NewsGroup with name "<<name<<" already exists in the current database."<<endl;
       }
       //DONE
 
@@ -88,15 +106,15 @@ int main(int argc, char* argv[]) {
 
       cout<<"Type in the number for the newsgroup that shall be deleted:"<<endl;
 
-      size_t number;
+      size_t number=validInput();
 
-      cin>>number;
+      
       c.sendDeleteNG(number);
       bool done = a.readAnsDeleteNG();
       if(done){
-      	cout<<"Newsgroup "<< number <<" deleted"<<endl;
+        cout<<"Newsgroup "<< number <<" deleted"<<endl;
       }else{
-      	cout<<"Deletion failed"<<endl;
+        cout<<"Deletion failed"<<endl;
       }
       //DONE
 
@@ -109,7 +127,7 @@ int main(int argc, char* argv[]) {
       c.sendListArt(number);
       vector<Article> g= a.readAnsListArt();
       for(Article& a: g){
-      	cout<<a.title<<endl;
+        cout<<a.title<<endl;
       }
       //DONE
 
@@ -131,24 +149,24 @@ int main(int argc, char* argv[]) {
 
       while(cin>>temp){
 
-	text+=temp;
+  text+=temp;
 
-	text+=" ";
+  text+=" ";
 
       }
 
 /*
-		c.sendCreateArt(0,name,author,text)
-		if(a.readCreateArt())	{
-			cout<<"Article "<< name <<" created"<<endl;;
-      	}else{
-	      	cout<<"Creation failed"<<endl;
-		}
+    c.sendCreateArt(0,name,author,text)
+    if(a.readCreateArt()) {
+      cout<<"Article "<< name <<" created"<<endl;;
+        }else{
+          cout<<"Creation failed"<<endl;
+    }
 */
 
     }else if(in=="6"){
 
-      cout<<"Type in the article number and the newsgroup number for the article that shall 			be deleted:"<<endl;
+      cout<<"Type in the article number and the newsgroup number for the article that shall       be deleted:"<<endl;
 
       size_t numberArticle;
 
@@ -158,21 +176,21 @@ int main(int argc, char* argv[]) {
 
       cin>>numberNG;
 
-	/*  c.sendDeleteArt(numberNG,numberArticle);
-	  auto res=a.readDeleteArt();
-	  if(res==ASD)
-	  {
-	 	 cout<<"Error?"
-	  }
-	  else 
-	  {
-		cout<<"Article deleted"<<endl;
-	  }*/
+  /*  c.sendDeleteArt(numberNG,numberArticle);
+    auto res=a.readDeleteArt();
+    if(res==ASD)
+    {
+     cout<<"Error?"
+    }
+    else 
+    {
+    cout<<"Article deleted"<<endl;
+    }*/
  
 
     }else if(in=="7"){
 
-      cout<<"Type in the article number and the newsgroup number for the article that shall 			be retrieved:"<<endl;
+      cout<<"Type in the article number and the newsgroup number for the article that shall       be retrieved:"<<endl;
 
       size_t numberArticle;
 
@@ -183,17 +201,17 @@ int main(int argc, char* argv[]) {
       cin>>numberNG;
 
 /*
-	 c.sendGetArt(numberNG, size_t numberArticle);   
+   c.sendGetArt(numberNG, size_t numberArticle);   
 
-		 try{
-			Article art = a.readGetArt();
-			cout<<"name:"<<art.title<<endl
- 				<<"author:"<<art.writer<<endl
-				<<contents<<endl;
-	 	 }   
-		 catch(WorldOnFireException eh)
-		{
-		}
+     try{
+      Article art = a.readGetArt();
+      cout<<"name:"<<art.title<<endl
+        <<"author:"<<art.writer<<endl
+        <<contents<<endl;
+     }   
+     catch(WorldOnFireException eh)
+    {
+    }
 */
 
     }else{
